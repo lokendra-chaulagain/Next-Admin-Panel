c7fcb7f38b3115c61e10f0d98eff10d770e94f3b
lokendra <lokendrachaulagain803@gmail.com>
2022-09-05 04:29:30 +0545

    four pages added

--- frontend/src/pages/coupons.tsx
+++ frontend/src/pages/coupons.tsx
@@ -1,11 +1,10 @@
 import Appbar from "../components/Appbar";
-import DashboardCard1 from "../components/DashboardCard1";
 import LeftAppBar from "../components/LeftBar";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import DashboardCardRow2 from "../components/DashboardCardRow2";
+import CouponTable from "../components/CouponTable";
 
-export default function Home() {
+export default function Category() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Appbar />
@@ -23,8 +22,7 @@ export default function Home() {
             marginLeft: "-100px",
           }}
         >
-          <DashboardCard1 />
-          <DashboardCardRow2 />
+          <CouponTable />
         </Grid>
       </Grid>
     </Box>
--- frontend/src/pages/product.tsx
+++ frontend/src/pages/product.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import Appbar from "../components/Appbar";
+import LeftAppBar from "../components/LeftBar";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import ProductTable from "../components/ProductTable";
 
 export default function Product() {
-  return <div></div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Appbar />
+      <Grid container spacing={1}>
+        <Grid item md={3}>
+          <LeftAppBar />
+        </Grid>
+        <Grid
+          container
+          item
+          md={8}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginLeft: "-100px",
+          }}
+        >
+          <ProductTable />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
