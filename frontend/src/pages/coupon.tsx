5e278d0ef0274bc2d7c400c1be08fd6f4824b1b5
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-09-23 08:09:30 +0545

    removed unnecessary code and pages

--- frontend/src/pages/coupon.tsx
+++ frontend/src/pages/coupon.tsx
@@ -1,14 +1,9 @@
-import Appbar from "../components/Topbar";
-import LeftAppBar from "../components/LeftBar";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import CouponTable from "../components/CouponTable";
+import Box from '@mui/material/Box'
 
 export default function Coupon() {
   return (
     <Box>
       <h1>Coupon</h1>
-      <CouponTable />
     </Box>
-  );
+  )
 }
--- frontend/src/pages/customer.tsx
+++ frontend/src/pages/customer.tsx
@@ -1,11 +1,5 @@
-import Box from "@mui/material/Box";
-import CustomerTable from "../components/CustomerTable";
+import Box from '@mui/material/Box'
 
 export default function Customer() {
-  return (
-    <Box>
-      <h1>Customer:data fetched using Redux ToolKit And RTK Query</h1>
-      <CustomerTable />
-    </Box>
-  );
+  return <Box>Customer</Box>
 }
